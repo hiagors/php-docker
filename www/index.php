4e0018e111c366ce2fd8c12423f3c689b7df9f1e
hiagors <hiago_reis@hotmail.com>
2021-06-02 14:28:33 -0300

    jsonUtil

--- www/index.php
+++ www/index.php
@@ -2,15 +2,20 @@
 
 use Validator\RequestValidator;
 use Util\RotasUtil;
+use Util\JsonUtil;
 
 include 'bootstrap.php';
 
 try{
     $validator = new RequestValidator(RotasUtil::getRotas());
     $retorno = $validator->processarRequest();
+
+    $jsonUtil = new JsonUtil();
+    $jsonUtil->
+    echo(json_encode($retorno));
     
 }catch(Exception $e){
-    echo $e->getMessage();
+    echo(json_encode($e->getMessage()));
 }
 
 
